86e6848aa248ba0c5a1cfae102faacb2e828535e
Marko Silla <marko.silla@gmail.com>
2025-02-26 02:31:56 +0200

    extract data from ehr_files

--- Script1_Fetch_Building_EHR_Data.py
+++ Script1_Fetch_Building_EHR_Data.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 def fetch_building_data(building_id):
     # URL of the API endpoint
@@ -27,14 +28,45 @@ def fetch_building_data(building_id):
         print(f"Error fetching data: {response.status_code}")
         return None
 
-def save_data_to_file(building_id, data):
-    filename = f"{building_id}.ehr.json"
+def save_data_to_file(building_id, data, folder="ehr_files"):
+    # Ensure the folder exists
+    os.makedirs(folder, exist_ok=True)
+    
+    # Construct the full file path
+    filename = os.path.join(folder, f"{building_id}.ehr.json")
+
+    # Save the file
     with open(filename, 'w') as file:
         json.dump(data, file, indent=4)
+
     print(f"Data saved to {filename}")
 
+# 222	marko.silla@taltech.ee	2025K	121363179	Pärnu	Rääma tn 9
+# 222	marko.silla@taltech.ee	2025K	121363182	Pärnu	Rääma tn 9a
+# 222	marko.silla@taltech.ee	2025K	121363185	Pärnu	Rääma tn 11
+# 223	marko.silla@taltech.ee	2025K	103014811	Pärnu	Mai 39
+# 223	marko.silla@taltech.ee	2025K	103015648	Pärnu	Mai45
+# 223	marko.silla@taltech.ee	2025K	103015786	Pärnu	Mai 43
+# 224	marko.silla@taltech.ee	2025K	104018086	Tartu	Mõisavahe 35
+# 224	marko.silla@taltech.ee	2025K	104018376	Tartu	Mõisavahe 38
+# 224	marko.silla@taltech.ee	2025K	104023804	Tartu	Mõisavahe 42
+# 225	marko.silla@taltech.ee	2025K	104018213	Tartu	Mõisavahe 43
+# 225	marko.silla@taltech.ee	2025K	104019313	Tartu	Mõisavahe 47
+# 225	marko.silla@taltech.ee	2025K	104036004	Tartu	Mõisavahe 39
+
 if __name__ == "__main__":
-    building_ids = ["101015900", "101020224"]
+    building_ids = ["121363179",
+"121363182",
+"121363185",
+"103014811",
+"103015648",
+"103015786",
+"104018086",
+"104018376",
+"104023804",
+"104018213",
+"104019313",
+"104036004"]
     for building_id in building_ids:
         print(f"Fetching data for Building ID: {building_id}")
         data = fetch_building_data(building_id)
