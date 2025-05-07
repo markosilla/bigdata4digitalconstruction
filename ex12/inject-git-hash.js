d28068370e48ada99bb91c798743e8e2fadaad36
Marko Silla <marko.silla@ria.ee>
2025-05-07 22:12:50 +0300

    Add git hash to index.html title

---
+++ ex12/inject-git-hash.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const fullHash = process.env.VERCEL_GIT_COMMIT_SHA || "dev";
+const hash = fullHash.substring(0, 7);
+const filePath = "src/index.html";
+const html = fs.readFileSync(filePath, "utf8");
+const result = html.replace("{{GIT_HASH}}", hash);
+fs.writeFileSync(filePath, result);
+
+console.log(`Injected git hash "${hash}" into ${filePath}`);
